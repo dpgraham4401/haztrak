86810662df14e6cec4d86cf2168a1d1018dc0126
David Paul Graham <dpgraham4401@gmail.com>
2024-06-29 21:27:10 -0400

    add UserRole, Role, and Authority model

--- server/apps/core/models.py
+++ server/apps/core/models.py
@@ -1,7 +1,9 @@
 import uuid
 
 from django.contrib.auth.models import AbstractUser
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class TrakUser(AbstractUser):
@@ -17,3 +19,22 @@ class TrakUser(AbstractUser):
         editable=False,
         default=uuid.uuid4,
     )
+
+
+class Authority(models.Model):
+    name = models.CharField(blank=False, max_length=50)
+    content_type = models.ForeignKey(
+        ContentType,
+        models.CASCADE,
+        verbose_name=_("content type"),
+    )
+
+
+class Role(models.Model):
+    name = models.CharField(blank=False, max_length=50)
+    authority = models.ManyToManyField(Authority)
+
+
+class UserRole(models.Model):
+    user = models.OneToOneField(TrakUser, on_delete=models.CASCADE)
+    role = models.ForeignKey(Role, on_delete=models.SET_NULL, null=True)
