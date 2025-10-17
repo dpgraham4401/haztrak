5953fce71a902aae87957f83e9aede0d5c438d0b
David Paul Graham <dpgraham4401@gmail.com>
2025-10-17 07:49:15 -0400

    wasteline schema add wasteline api endpoints to v2 API

--- server/haztrak/api.py
+++ server/haztrak/api.py
@@ -7,6 +7,7 @@ from manifest.api import router as manifest_router
 from org.api import router as org_router
 from profile.api import router as profile_router
 from rcrasite.api import router as rcrasite_router
+from wasteline.api import router as wasteline_router
 
 api_auth = []
 
@@ -21,6 +22,7 @@ api = NinjaAPI(
 rcrainfo_router = Router()
 rcrainfo_router.add_router("", rcrasite_router)
 rcrainfo_router.add_router("", manifest_router)
+rcrainfo_router.add_router("", wasteline_router)
 
 api.add_router("", core_router)
 api.add_router("", org_router)
