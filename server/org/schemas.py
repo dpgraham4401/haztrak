6c14e0a981b7d06dc1e0e83d0effc8a01dd09ec8
David Paul Graham <dpgraham4401@gmail.com>
2025-10-16 22:12:53 -0400

    initial OrgSchema

---
+++ server/org/schemas.py
@@ -0,0 +1,22 @@
+"""schemas for Org and Site."""
+
+import uuid
+
+from ninja import Schema
+from pydantic import ConfigDict, Field
+from pydantic.alias_generators import to_camel
+
+
+class OrgSchema(Schema):
+    """Haztrak Organization schema."""
+
+    id: uuid.UUID
+    name: str
+    slug: str
+    is_rcrainfo_integrated: bool = Field(alias="rcrainfoIntegrated")
+
+    model_config = ConfigDict(
+        from_attributes=True,
+        populate_by_name=True,
+        alias_generator=to_camel,
+    )
