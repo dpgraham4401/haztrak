3bfaaebc5ac970fb7e109ed304f4ab9e96d743b6
David Paul Graham <dpgraham4401@gmail.com>
2025-10-22 21:19:27 -0400

    RCRAInfo access schema for retrieving profile information from RCRAinfo

--- server/profile/schemas/rcrainfo_profile.py
+++ server/profile/schemas/rcrainfo_profile.py
@@ -3,7 +3,7 @@
 from typing import Annotated
 
 from ninja import ModelSchema, Schema
-from pydantic import ConfigDict, Field
+from pydantic import ConfigDict, Field, field_validator
 from pydantic.alias_generators import to_camel
 
 from profile.models import RcrainfoSiteAccess
@@ -59,3 +59,27 @@ class RcrainfoProfileSchema(Schema):
         populate_by_name=True,
         alias_generator=to_camel,
     )
+
+
+class RcrainfoModuleAccessSchema(Schema):
+    """Represents a single permission entry from RCRAInfo JSON."""
+
+    module: str
+    level: str | bool
+
+    @field_validator("level", mode="before")
+    @classmethod
+    def validate_level(cls, level: str) -> str | bool:
+        """Convert module permission level to our internal value."""
+        site_management_strings = ["Active", "Inactive"]
+        if level in site_management_strings:
+            return level == "Active"
+        return level
+
+
+class RcrainfoSiteAccessSchema(Schema):
+    """Top-level RCRAInfo site access schema."""
+
+    site_id: Annotated[str, Field(..., alias="siteId")]
+    site_name: Annotated[str, Field(..., alias="siteName")]
+    permissions: list[RcrainfoModuleAccessSchema]
