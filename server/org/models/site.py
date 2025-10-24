2a44b9a9b95a23b9cd2bfa8de9a94b5d0e5d79b6
David Paul Graham <dpgraham4401@gmail.com>
2025-10-24 07:55:32 -0400

    convert org.site to use an UUID for its primary key

--- server/org/models/site.py
+++ server/org/models/site.py
@@ -1,6 +1,7 @@
 """Organization and Site models."""
 
 from typing import TYPE_CHECKING
+from uuid import uuid4
 
 from django.core.validators import MinLengthValidator
 from django.db import models
@@ -68,6 +69,12 @@ class SiteManager(QuerySet):
 class Site(models.Model):
     """The site entity represents a physical location that is a part of an organization."""
 
+    id = models.UUIDField(
+        unique=True,
+        editable=False,
+        primary_key=True,
+        default=uuid4,
+    )
     name = models.CharField(
         verbose_name="site alias",
         max_length=200,
