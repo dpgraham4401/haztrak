f578202becdc6dd8939859e2b3fce711e439d437
David Paul Graham <dpgraham4401@gmail.com>
2025-10-30 08:30:07 -0400

    remove redundant

--- server/core/api.py
+++ server/core/api.py
@@ -2,42 +2,24 @@
 
 from http import HTTPStatus
 
+from django.http import HttpRequest
 from django_celery_results.models import TaskResult
 from ninja import Router
 from ninja.errors import AuthenticationError
 from rest_framework.exceptions import ValidationError
 
-from core.models import TrakUser
 from core.schemas import ErrorSchema, TaskStatusSchema, TaskUnknownSchema, TrakUserSchema
 from core.services import get_task_status
 
 router = Router(tags=["Core"], by_alias=True, exclude_none=True)
 
 
-@router.get("/users", response=list[TrakUserSchema])
-def list_users(request):
-    """Endpoint to list users."""
-    return TrakUser.objects.all()
-
-
 @router.get(
     "/users/me",
     response=TrakUserSchema,
 )
-def get_current_user(request) -> TrakUser:
+def get_current_user(request: HttpRequest):
     """Get the currently authenticated user (mirrors DRF GetCurrentTrakUserView)."""
-    user = request.user
-    if user.is_anonymous:
-        raise AuthenticationError(message="Authentication required to access this endpoint.")
-    return user
-
-
-@router.get(
-    "/user/current-user",
-    response=TrakUserSchema,
-)
-def get_current_user_legacy_path(request) -> TrakUser:
-    """Alias to match DRF route name and path structure under v2 API."""
     user = request.user
     if user.is_anonymous:
         raise AuthenticationError(message="Authentication required to access this endpoint.")
